27c5016bf8e5127f16daedb26460ddd61df12127
Bebetta Motondo <claramotondo2@yahoo.fr>
2023-03-27 18:00:25 -0500

    Linq 1

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> favoriteMovies = new List<string>() { "How to lose a guy in ten days", "Just go with it", "The proposal" };
+
+            favoriteMovies.Add("My best friend wedding");
+            favoriteMovies.Add("Grown up");
+
+            var orderedMovies = favoriteMovies.OrderBy(name => name.Length);
+
+            foreach (var movie in orderedMovies)
+            {
+                Console.WriteLine(movie);
+            }
         }
     }
 }
